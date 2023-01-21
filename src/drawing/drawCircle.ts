c6c4151dfc1e4050475b65ce50aafa7e1c85c378
marina.shilo <marina.shilo@degree53.com>
2023-01-21 14:19:01 +0300

    refactor: rewrite commands obj

--- src/drawing/drawCircle.ts
+++ src/drawing/drawCircle.ts
@@ -6,21 +6,21 @@ import { getScreenParam } from './helpers';
 const CIRCLE_STEP = 0.001;
 
 export const drawCircle = async (coordinates: string[]): Promise<DRAWING_COMMANDS_NAME> => {
-    const [ radius ] = coordinates;
+	const [ radius ] = coordinates;
 	const { height: screenHeight, width: screenWidth } = await getScreenParam();
 	const { x: currentX, y: currentY } = await mouse.getPosition();
-	const isCircleCanBeDrawn = radius
-		&& (currentX + 2 * parseInt(radius)) < screenWidth
-		&& (currentY + 2 * parseInt(radius)) < screenHeight	
-		&& (currentX - parseInt(radius)) > 0
-		&& (currentY - parseInt(radius)) > 0;
-		
+
+	const isCircleCanBeDrawn = radius &&
+		(currentX + 2 * parseInt(radius)) < screenWidth &&
+		(currentY + 2 * parseInt(radius)) < screenHeight &&
+		(currentX - parseInt(radius)) > 0 && (currentY - parseInt(radius)) > 0;
+
 	if (!isCircleCanBeDrawn) {
 		throw new NotCorrectParamError();
 	}
 	
 	mouse.config.mouseSpeed = MOUSE_SPEED;
-	
+
 	const centerX = currentX + parseInt(radius);
 	const centerY = currentY;
 	
@@ -29,7 +29,6 @@ export const drawCircle = async (coordinates: string[]): Promise<DRAWING_COMMAND
 	for (let i = 0; i <= 2 * Math.PI; i += CIRCLE_STEP) {
 		const moveX = centerX - parseInt(radius) * Math.cos(i);
 		const moveY = centerY - parseInt(radius) * Math.sin(i);
-		
 		await mouse.move(straightTo(new Point(moveX, moveY)));
 	}
 	
